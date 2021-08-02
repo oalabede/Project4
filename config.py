3ff50bbdecba25853b4be0dd9bb6ea081b290468
Toyin <oa35@njit.edu>
2021-08-02 15:15:37 -0400

    Handle User Accounts & Authentication in Flask with Flask Login

--- config.py
+++ config.py
@@ -1,14 +1,32 @@
 """App configuration."""
-from os import environ
+from os import environ, path
+
 import redis
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
 
 
 class Config:
-    """Set Flask configuration vars from .env file."""
+    """Set Flask configuration variables from .env file."""
 
-    SECRET_KEY = environ.get('SECRET_KEY')
-    FLASK_APP = environ.get('FLASK_APP')
-    FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_APP = 'wsgi.py'
+    FLASK_ENV = environ.get("FLASK_ENV")
+    SECRET_KEY = environ.get("SECRET_KEY")
 
-    SESSION_TYPE = environ.get('SESSION_TYPE')
-    SESSION_REDIS = redis.from_url(environ.get('SESSION_REDIS'))
+    REDIS_URI = environ.get("SESSION_REDIS")
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.from_url(REDIS_URI)
+
+    LESS_BIN = environ.get("LESS_BIN")
+    ASSETS_DEBUG = environ.get('ASSETS_DEBUG')
+    LESS_RUN_IN_DEBUG = environ.get('LESS_RUN_IN_DEBUG')
+
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+    COMPRESSOR_DEBUG = environ.get('COMPRESSOR_DEBUG')
+
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
